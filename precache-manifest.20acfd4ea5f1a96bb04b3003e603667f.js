a1521fac988f88776c90bd8c3825fdacbfa49be7
abhimaurya <arm20493@gmail.com>
2019-12-22 11:32:40 +0530

    Updates

---
+++ precache-manifest.20acfd4ea5f1a96bb04b3003e603667f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9cdbaf253019ae39dfaa4c86f1b8749",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c412a1543c796a3c7d0",
+    "url": "/static/css/main.1e72d898.chunk.css"
+  },
+  {
+    "revision": "bab663dbe233df65d422",
+    "url": "/static/js/2.40efff77.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.40efff77.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6c412a1543c796a3c7d0",
+    "url": "/static/js/main.fbe2524d.chunk.js"
+  },
+  {
+    "revision": "3eb3e9a6f0a97fb2528b",
+    "url": "/static/js/runtime-main.91aea62a.js"
+  }
+]);
